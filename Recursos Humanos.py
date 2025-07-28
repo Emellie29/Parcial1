e8131336a8bcac349e883fd82f520569675cfe92
Emellie29 <emellie.lopez@udeo.edu.gt>
2025-07-28 10:05:24 -0600

    primera parte parcial

---
+++ Recursos Humanos.py
@@ -0,0 +1,6 @@
+class empleado():
+    def __init__(self, nombre, apellido, departamento, antiguedad):
+        self.nombre = nombre
+        self.apellido = apellido
+        self.departamento = departamento
+        self.antiguedad = antiguedad
